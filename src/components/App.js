069398043f94a92b2c5a13ec6ef4a55962192264
pratik kumar <kumarpratik861@gmail.com>
2020-11-17 00:43:39 +0530

    assignment done done the  assignment so update the repo

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name">online donation platform.</h1>
+      <p data-ns-test="project-description">
+        This is a platform where people can donate for underprivileged people
+        with their unused/unwanted product
+        like(Cloths,medicine,footwear,books,study material and others....) which
+        they have but of no use and they are wiling to donate but lacks with
+        time or the place they like to donate is far from their place so they
+        are delaying. these person can use my platform to donate any kind of
+        product or any other assistance and we will deliver to them on behalf of
+        them. for product we will have a form where they can fill the
+        information and product they are willing to donate and best time to pick
+        up and anyone from my team will pick-up the product and we will ensure
+        that it reaches to correct people. we will also informer the doner with
+        the details whom we have doanted once the donation reaches to the
+        needy.....
+      </p>
+    </>
+  );
+};
 
 export default App;
